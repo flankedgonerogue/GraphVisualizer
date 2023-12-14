af5672e52fd9d76895cf23bf3d6aa05dd022754c
Abdur Rahman Goraya <abdurrahmangoraya@outlook.com>
2023-12-14 21:25:49 +0500

    Create Graph Class
    Took 56 seconds

---
+++ include/Graph.h
@@ -0,0 +1,12 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+namespace Flanky {
+
+class Graph {
+
+};
+
+} // Flanky
+
+#endif //GRAPH_H
---
+++ src/Graph.cpp
@@ -0,0 +1,4 @@
+#include "Graph.h"
+
+namespace Flanky {
+} // Flanky
